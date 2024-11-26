ac948a6fd86dfded2e59b61b12fd21106f79f852
Skyiesac <jainsachi1202@gmail.com>
2024-11-26 15:00:52 +0530

    added throttling

--- xpensateapp/settings.py
+++ xpensateapp/settings.py
@@ -81,7 +81,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
          'rest_framework.authentication.SessionAuthentication', 
           'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+     "DEFAULT_THROTTLE_RATES": {
+        'anon': '30/hour',
+        'user': '30/hour'
+    },
 }
 ROOT_URLCONF = 'xpensateapp.urls'
 
